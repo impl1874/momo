4b6359a938577e91d1f52f61f1b881beaddddc73
Sheldon <sheldon@163.com>
2022-05-12 16:18:04 +0800

    add sdl2 test

---
+++ test/sdl2/sdl2test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(void)
+{
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		printf("Couldn't initialize SDL - %s\n", SDL_GetError());
+	}
+	else
+	{
+		printf("Success init SDL\n");
+	}
+	return 0;
+}
